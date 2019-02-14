3b2850d3f792d60696219fce12000304be0d9d66
zg <zhanggang5>
2019-02-14 18:07:13 +0800

    yml文件

--- src/main/java/com/example/demo/Application.java
+++ src/main/java/com/example/demo/Application.java
@@ -1,12 +1,30 @@
 package com.example.demo;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.sql.DataSource;
+import java.sql.Connection;
+
 @SpringBootApplication
-public class Application {
+@Slf4j
+public class Application implements CommandLineRunner {
+
+    @Autowired
+    private DataSource dataSource;
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info(dataSource.toString());
+        Connection connection = dataSource.getConnection();
+        log.info(connection.toString());
+        connection.close();
     }
 }
